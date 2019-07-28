8b59b9dc90cbb97668ce16c03fe8499ae8a480ab
Josue Gabuardi <josuegabuardi@gmail.com>
2019-07-28 00:01:23 -0600

    add APP_INITIALIZER

--- src/app/home/community-selector/community-selector.component.ts
+++ src/app/home/community-selector/community-selector.component.ts
@@ -1,74 +1,78 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {PickerController} from '@ionic/angular';
+import {LocationsProviderService} from '../../services/locations-provider.service';
 
 @Component({
-  selector: 'app-community-selector',
-  templateUrl: './community-selector.component.html',
-  styleUrls: ['./community-selector.component.scss'],
+    selector: 'app-community-selector',
+    templateUrl: './community-selector.component.html',
+    styleUrls: ['./community-selector.component.scss'],
 })
 export class CommunitySelectorComponent implements OnInit {
 
-  constructor(private pickerCtrl: PickerController) { }
+    constructor(private pickerCtrl: PickerController, private locationsProvider: LocationsProviderService) {
+        console.log(locationsProvider.getProvinces());
+    }
 
-  ngOnInit() {}
+    ngOnInit() {
+    }
 
-  async showPicker() {
-    let opts = {
-      buttons: [
-        {
-          text: 'Cancel',
-          role: 'cancel'
-        },
-        {
-          text: 'Done'
-        }
-      ],
-      columns: [
-        {
-          name: 'framework',
-          options: [
-            { text: 'Angular', value: 'A' },
-            { text: 'Vue', value: 'B' },
-            { text: 'React', value: 'C' }
-          ]
-        }
-      ]
-    };
-    let picker = await this.pickerCtrl.create(opts);
-    picker.present();
-    picker.onDidDismiss().then(async data => {
-      this.showAnotherPicker();
-    });
-  }
+    async showPicker() {
+        let opts = {
+            buttons: [
+                {
+                    text: 'Cancel',
+                    role: 'cancel'
+                },
+                {
+                    text: 'Done'
+                }
+            ],
+            columns: [
+                {
+                    name: 'framework',
+                    options: [
+                        {text: 'Angular', value: 'A'},
+                        {text: 'Vue', value: 'B'},
+                        {text: 'React', value: 'C'}
+                    ]
+                }
+            ]
+        };
+        let picker = await this.pickerCtrl.create(opts);
+        picker.present();
+        picker.onDidDismiss().then(async data => {
+            this.showAnotherPicker();
+        });
+    }
 
 
-  async showAnotherPicker() {
-    let opts = {
-      buttons: [
-        {
-          text: 'Cancel',
-          role: 'cancel'
-        },
-        {
-          text: 'Done'
-        }
-      ],
-      columns: [
-        {
-          name: 'Team',
-          options: [
-            { text: 'Fnatic', value: 'A' },
-            { text: 'G2 Esports', value: 'B' },
-            { text: 'Origen', value: 'C' }
-          ]
-        }
-      ]
-    };
-    let picker = await this.pickerCtrl.create(opts);
-    picker.present();
-    picker.onDidDismiss().then(async data => {
-      console.log(data);
-    });
-  }
+    async showAnotherPicker() {
+        let opts = {
+            buttons: [
+                {
+                    text: 'Cancel',
+                    role: 'cancel'
+                },
+                {
+                    text: 'Done'
+                }
+            ],
+            columns: [
+                {
+                    name: 'Team',
+                    options: [
+                        {text: 'Fnatic', value: 'A'},
+                        {text: 'G2 Esports', value: 'B'},
+                        {text: 'Origen', value: 'C'}
+                    ]
+                }
+            ]
+        };
+        let picker = await this.pickerCtrl.create(opts);
+        picker.present();
+        picker.onDidDismiss().then(async data => {
+            console.log(data);
+        });
+    }
 
 } // COMPONENT ENDS
--- src/app/services/locations-provider.service.ts
+++ src/app/services/locations-provider.service.ts
@@ -1,11 +1,59 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 
 export class LocationsProviderService {
 
-  constructor(private http: Http) { }
+    // ---------------------------------------------------
+    // SERVICE PROPERTIES
+    // ---------------------------------------------------
+    private provinces: object;
+    private cities: object;
+    private towns: object;
+
+    // ---------------------------------------------------
+
+    constructor(private http: HttpClient) {
+    }
+
+    // ---------------------------------------------------
+    // GET -> RETURN THE PROVINCES PROPERTY
+    // ---------------------------------------------------
+    public getProvinces(): object {
+        return this.provinces;
+    } // ENDS
+
+
+    // ---------------------------------------------------
+    // GET -> RETURN CITIES PROPERTY
+    // ---------------------------------------------------
+    public getCities(): object {
+        return this.cities;
+    } // ENDS
+
+
+    // ---------------------------------------------------
+    // GET -> RETURN TOWNS PROPERTY
+    // ---------------------------------------------------
+    public getTowns(): object {
+        return this.towns;
+    } // ENDS
+
+    // ---------------------------------------------------
+    // METHOD -> RETURN TOWNS PROPERTY
+    // ---------------------------------------------------
+    loadProvinces() {
+        return new Promise((resolve) => {
+            this.http
+                .get('https://resilapp.mybluemix.net/api/towns/provinces')
+                .subscribe(response => {
+                    this.provinces = response;
+                    resolve(true);
+                });
+        });
+    }
 
 } // SERVICE ENDS
