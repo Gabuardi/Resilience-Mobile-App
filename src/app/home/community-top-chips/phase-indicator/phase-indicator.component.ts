c3fa65b702180f817e935782b58b32584c7acb14
Josue Gabuardi <josuegabuardi@gmail.com>
2019-07-28 14:33:59 -0600

    generate phase indicator component

---
+++ src/app/home/community-top-chips/phase-indicator/phase-indicator.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-phase-indicator',
+  templateUrl: './phase-indicator.component.html',
+  styleUrls: ['./phase-indicator.component.scss'],
+})
+export class PhaseIndicatorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
