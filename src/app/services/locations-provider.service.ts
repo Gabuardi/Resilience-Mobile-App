9ad4bb1bb1417803b5cf6db7c8a7943c6966e1c4
Josue Gabuardi <josuegabuardi@gmail.com>
2019-07-27 23:30:39 -0600

    generate location provider service

---
+++ src/app/services/locations-provider.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class LocationsProviderService {
+
+  constructor(private http: Http) { }
+
+} // SERVICE ENDS
