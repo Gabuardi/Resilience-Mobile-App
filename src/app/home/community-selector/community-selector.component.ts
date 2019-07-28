d22d2dea3d655a0d0323f09b70cdce555753d7d9
Josue Gabuardi <josuegabuardi@gmail.com>
2019-07-27 19:22:05 -0600

    generate new component

---
+++ src/app/home/community-selector/community-selector.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-community-selector',
+  templateUrl: './community-selector.component.html',
+  styleUrls: ['./community-selector.component.scss'],
+})
+export class CommunitySelectorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
