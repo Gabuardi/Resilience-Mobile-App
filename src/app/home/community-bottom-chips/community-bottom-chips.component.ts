988ea01a1d2a6d7057b295996edce5d19befdd15
Josue Gabuardi <josuegabuardi@gmail.com>
2019-07-28 21:50:04 -0600

    generate new component

---
+++ src/app/home/community-bottom-chips/community-bottom-chips.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-community-bottom-chips',
+  templateUrl: './community-bottom-chips.component.html',
+  styleUrls: ['./community-bottom-chips.component.scss'],
+})
+export class CommunityBottomChipsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
