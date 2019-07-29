7bef9e34f00b652377664e66a15c443128ba32e5
Josue Gabuardi <josuegabuardi@gmail.com>
2019-07-28 23:58:19 -0600

    last fixes

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import {Component} from '@angular/core';
+import {ResilienceService} from '../services/resilience.service';
 
 @Component({
     selector: 'app-home',
@@ -13,13 +14,33 @@ export class HomePage {
     phase: number;
     resilienceProgress: number;
     phaseProgress: number;
+
     // --------------------------------------------------------------------------------------
+
+    constructor(private resilienceService: ResilienceService) {
+    }
 
     // --------------------------------------------------------------------------------------
     // METHOD -> IS CALLED WHEN COMMUNITY IS SELECTED AND BRING DATA FROM COMMUNITY SELECTED
     // --------------------------------------------------------------------------------------
     communitySelectedEventHandler(object) {
+
+        // PRINT IN CONSOLE THE OBJECT DATA
         console.log(`${object.province} - ${object.city} - ${object.town}`);
+
+        // CALL GET COMMUNITY RESILIENCE ENDPOINT
+        this.resilienceService.getCommunityResilience(object.province, object.city, object.town).subscribe(value => {
+
+            // PRINT IN CONSOLE THE DATA
+            console.log(value);
+
+            // SAVE THE DATA IN COMPONENT PROPERTIES
+            this.phase = value.RESILIENCIA.RESILIENCIA.stage;
+            this.resilienceProgress = value.RESILIENCIA.resilience_total_level;
+            this.phaseProgress = value.RESILIENCIA.resilience_stage_level;
+
+        });
+
     } // METHOD END
 
 } // COMPONENT ENDS
