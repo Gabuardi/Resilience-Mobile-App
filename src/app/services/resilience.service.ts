81d56de06b99b932670cca34f3401cb346656462
Josue Gabuardi <josuegabuardi@gmail.com>
2019-07-28 22:45:40 -0600

    add get community resilience endpoint

--- src/app/services/resilience.service.ts
+++ src/app/services/resilience.service.ts
@@ -1,9 +1,32 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {environment} from '../../environments/environment';
+import {Observable} from 'rxjs';
+import {map} from 'rxjs/operators';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class ResilienceService {
 
-  constructor() { }
-}
+    // ------------------------------------------------------------------
+    // SERVICE PROPERTIES
+    // ------------------------------------------------------------------
+    private URL = `${environment.API}/communities`;
+
+    // ------------------------------------------------------------------
+
+    constructor(private httpClient: HttpClient) {
+    }
+
+    // ------------------------------------------------------------------
+    // GET -> GET THE RESILIENCE OBJECT FROM AN COMMUNITY
+    // ------------------------------------------------------------------
+    getCommunityResilience(province: string, city: string, town: string): Observable<any> {
+        return this.httpClient.get(`${this.URL}/${province}/${city}/${town}`).pipe(
+            map((res: any) => res)
+        );
+    } // GET ENDS
+
+
+} // SERVICE ENDS
